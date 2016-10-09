230456d740cabf7eb48d46ca99ef6e703b02047f
furmancapital <furmancapital@gmail.com>
2016-10-09 19:27:32 -0400

    Initial check in

---
+++ TikiORM/TikiORM.Core/DbParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurmanCapital.TikiORM.Core
+{
+    public class DbParameter
+    {
+        public DbParameter (string parameterName, object parameterValue)
+        {
+
+        }
+    }
+}
---
+++ TikiORM/TikiORM.Core/QueryUnknownParametersException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurmanCapital.TikiORM.Core
+{
+    /// <summary>
+    /// This exception indicates a situation in which we have unknown parameters (i.e. parameters are either passed in that are NOT
+    /// in the query, or we have parameters the query expects that have NOT been passed in)
+    /// </summary>
+    public class QueryUnknownParametersException : Exception
+    {
+    }
+}

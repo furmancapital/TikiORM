1edef0c0f90ed0dc47cfb82dd9c54bbe221996bf
furmancapital <furmancapital@gmail.com>
2016-10-16 15:46:53 -0400

    fixed bad merge

---
+++ TikiORM/TikiORM.Core/Mappers/ColumnNameToObjectResultMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurmanCapitalTechGroup.TikiORM.Core.Mappers
+{
+    public class ColumnNameToObjectResultMapper<TResult> : IResultMapper<TResult>
+    {
+        public TResult MapResult(IDataReader dataReader, QueryResultStructure resultStructure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
